1077295f41f37173596065b8df9d96a66d306dd0
Steven Schwedt <sschwedt@gmail.com>
2023-11-09 09:57:55 -0700

    Initial Commit

---
+++ NeoNeo5/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NeoNeo5
+//
+//  Created by Steven Schwedt on 11/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NeoNeo5/NeoNeo5App.swift
@@ -0,0 +1,17 @@
+//
+//  NeoNeo5App.swift
+//  NeoNeo5
+//
+//  Created by Steven Schwedt on 11/9/23.
+//
+
+import SwiftUI
+
+@main
+struct NeoNeo5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
